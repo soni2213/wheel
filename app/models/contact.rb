3b073677c190a29566b6a961e20057f49684359f
Soni Sahu <soni.sahu@bigbinary.com>
2021-09-02 12:23:38 +0530

    Remove BE dependency

--- app/models/contact.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Contact < ApplicationRecord
-  has_many :tasks
-
-  validates :name, :email, :mobile, presence: true
-  validates :email, uniqueness: true
-end
